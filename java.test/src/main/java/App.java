e185e76f9a5d04f783324b7199b0232a9438cce5
m.stock <36995969+maxones25@users.noreply.github.com>
2021-10-04 18:48:45 +0200

    init

---
+++ java.test/src/main/java/App.java
@@ -0,0 +1,13 @@
+
+
+public class App {
+
+	public static double add(double a, double b) {
+		return a + b;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("5 + 6 = " + add(5, 6));
+	}
+
+}
---
+++ java.test/src/test/java/AppTest.java
@@ -0,0 +1,14 @@
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void testAdd() {
+		assertEquals(11, (int) App.add(5, 6));
+	}
+
+}
